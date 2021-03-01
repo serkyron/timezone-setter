dee5bfa2f760606a62ed8b958aa8feb77b4e87ae
Sergei Krivosheenko <serkyron@gmail.com>
2021-03-01 16:03:15 +0300

    path to executabled is changed to use __dirname

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports.setTimezone = function(timezone) {
 			reject(new Error("Invalid timezone"));
 		}
 
-		execFile(`./node_modules/timezone-setter/bin/win32/timezone.exe`, [timezone], {windowsHide: true}, (error, stdout, stderr) => {
+		execFile(`${__dirname}\\bin\\win32\\timezone.exe`, [timezone], {windowsHide: true}, (error, stdout, stderr) => {
 			if (error) {
 				reject(error);
 			}
